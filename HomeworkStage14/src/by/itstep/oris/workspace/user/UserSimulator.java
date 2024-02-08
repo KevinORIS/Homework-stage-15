2142e67f8f0518e02aa0f37f1c81e245879b8c7d
User <User@User-PC>
2024-02-08 16:22:45 +0300

    Add UserSimulation util class

--- HomeworkStage14/src/by/itstep/oris/workspace/user/UserSimulator.java
+++ HomeworkStage14/src/by/itstep/oris/workspace/user/UserSimulator.java
@@ -1,5 +1,48 @@
 package by.itstep.oris.workspace.user;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class UserSimulator {
 
+	public static final Random RANDOM;
+	public static final Scanner SCANNER;
+
+	static {
+		RANDOM = new Random();
+		SCANNER = new Scanner(System.in);
+	}
+
+	public static int inputRandomNumber(int maxBound, int minBound) {
+		if (maxBound < minBound) {
+			int t = maxBound;
+			maxBound = minBound;
+			minBound = t;
+		}
+		return RANDOM.nextInt(maxBound - minBound + 1) + minBound;
+	}
+
+	public static int[] inputRandomNumbers(int maxBound, int minBound, int[] array) {
+		if (maxBound < minBound) {
+			int t = maxBound;
+			maxBound = minBound;
+			minBound = t;
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = RANDOM.nextInt(maxBound - minBound + 1) + minBound;
+		}
+		return array;
+	}
+
+	public static int inputSpecificNumber() {
+		return SCANNER.nextInt();
+	}
+
+	public static int[] inputSpecificNumbers(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = SCANNER.nextInt();
+		}
+		return array;
+	}
 }
