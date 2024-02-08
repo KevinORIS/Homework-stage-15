b46a6a92b396d3497a199c66a169b54d8b7aa82d
User <User@User-PC>
2024-02-08 16:27:44 +0300

    Add Main class for task 1 level a

--- HomeworkStage14/src/by/itstep/oris/workspace/levela/task01/controller/Main.java
+++ HomeworkStage14/src/by/itstep/oris/workspace/levela/task01/controller/Main.java
@@ -1,7 +1,23 @@
 package by.itstep.oris.workspace.levela.task01.controller;
 
+import by.itstep.oris.workspace.levela.task01.logic.VectorZeroCounter;
+import by.itstep.oris.workspace.levela.task01.view.VectorUI;
+import by.itstep.oris.workspace.user.UserSimulator;
+
 public class Main {
 	public static void main(String[] args) {
+		int size = 10;
 		
+		int[] vector = new int[size];
+		
+		int maxBound = 10;
+		
+		int minBound = -10;
+		
+		vector = UserSimulator.inputRandomNumbers(maxBound, minBound, vector);
+		
+		int count = VectorZeroCounter.countZeroElements(vector);
+		
+		VectorUI.getResult(count, vector);
 	}
 }
