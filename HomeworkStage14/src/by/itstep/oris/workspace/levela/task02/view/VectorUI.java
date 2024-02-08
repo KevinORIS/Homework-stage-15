3a8c9743bfcf44853cbeebcedfb2447d02c881c0
User <User@User-PC>
2024-02-08 16:40:47 +0300

    Add VectorUI class for task 2 level a

--- HomeworkStage14/src/by/itstep/oris/workspace/levela/task02/view/VectorUI.java
+++ HomeworkStage14/src/by/itstep/oris/workspace/levela/task02/view/VectorUI.java
@@ -1,11 +1,11 @@
-package by.itstep.oris.workspace.levela.task01.view;
+package by.itstep.oris.workspace.levela.task02.view;
 
 import java.util.Arrays;
 
 public class VectorUI {
 	public static void getResult(int count, int... vector) {
 		System.out.println(String.format
-				("Count of zero elements in %s is: %d", 
+				("Count of negative elements in %s is: %d", 
 						Arrays.toString(vector), count));
 	}
 }
