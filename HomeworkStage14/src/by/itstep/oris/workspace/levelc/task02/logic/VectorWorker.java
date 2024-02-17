a6e2e7b1171009cbef3bbeb69274499bb3ba82df
User <User@User-PC>
2024-02-17 23:47:17 +0300

    Add VectorWorker class task 2 level c

---
+++ HomeworkStage14/src/by/itstep/oris/workspace/levelc/task02/logic/VectorWorker.java
@@ -0,0 +1,29 @@
+package by.itstep.oris.workspace.levelc.task02.logic;
+
+public class VectorWorker {
+	public static int countElMoreThanGeomAvrg(int... vector) {
+		if(vector == null) {
+			return -1;
+		}
+		
+		double average = findAvrg(vector);
+				
+		int count = 0;
+		
+		for(int el : vector) {
+			if(el > average) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	private static double findAvrg(int... vector) {
+		double average = 1;
+		
+		for (int el : vector) { 
+			  average *= el; 
+		} 
+		return Math.pow(average, 1 / vector.length);
+	}
+}
