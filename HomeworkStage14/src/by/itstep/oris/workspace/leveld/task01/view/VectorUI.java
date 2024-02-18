37905cca0e787c890fae843320e471c36e1419b3
User <User@User-PC>
2024-02-18 10:33:22 +0300

    Add VectorUI task 1 level d

--- HomeworkStage14/src/by/itstep/oris/workspace/leveld/task01/view/VectorUI.java
+++ HomeworkStage14/src/by/itstep/oris/workspace/leveld/task01/view/VectorUI.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class VectorUI {
 	public static void getResult(int sum, int... vector) {
 		System.out.println(String.format
-				("Sum of elements between min and max elements in %s is: %d", 
+				("Sum of elements between min and max elements in %s is: %d",
 						 Arrays.toString(vector), sum));
 	}
 }
