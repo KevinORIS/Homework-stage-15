5c5404be922da50dbc6328c06304dd8b7765ee54
User <User@User-PC>
2024-03-02 11:48:56 +0300

    Add findMaxAboveDiagonal method

--- HomeworkStage15/src/by/itstep/oris/workspace/logic/MatrixManager.java
+++ HomeworkStage15/src/by/itstep/oris/workspace/logic/MatrixManager.java
@@ -1,5 +1,17 @@
 package by.itstep.oris.workspace.logic;
 
 public class MatrixManager {
-
+	// O(N*N)
+	public static int findMaxAboveDiagonal(int[][] matrix) {
+		int max = matrix[0][0];
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = i + 1; j < matrix[i].length; j++) {
+				if (matrix[i][j] > max) {
+					max = matrix[i][j];
+				}
+			}
+		}
+		return max;
+	}
 }
